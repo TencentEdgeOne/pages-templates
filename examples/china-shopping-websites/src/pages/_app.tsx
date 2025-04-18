c694a1cbbbc13d4751f8969752f577e313555859
sss <sss@gmail.com>
2025-04-18 11:21:11 +0800

    seo

--- examples/china-shopping-websites/src/pages/_app.tsx
+++ examples/china-shopping-websites/src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { NextSeo } from 'next-seo';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextSeo
+        title="Chinese Shopping Websites"
+        description="An one-stop navigation platform for Chinese e-commerce, integrating comprehensive resources across cross-border trade, fashion accessories, digital electronics, and other categories."
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
