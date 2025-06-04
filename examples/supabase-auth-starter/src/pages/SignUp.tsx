c383f7fc5bda59fce2e6e69b6b0c30432e654dde
sss <sss@gmail.com>
2025-06-04 16:15:16 +0800

    errorfix

--- examples/supabase-auth-starter/src/pages/SignUp.tsx
+++ examples/supabase-auth-starter/src/pages/SignUp.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Link, useNavigate } from "react-router-dom";
-import { NavBar } from "@/components/NavBar";
 import { getURL } from "../lib/utils";
 
 export function SignUp() {
