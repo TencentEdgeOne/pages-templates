401f08cf6f213791389c0239dfcb72d06487c491
corbinlin <corbinlin@tencent.com>
2025-07-04 14:40:13 +0800

    feat : trtc-ar utils

---
+++ examples/trtc-ar/utils/type.d.ts
@@ -0,0 +1,13 @@
+declare module 'tencentcloud-webar' {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  export const ArSdk: any;
+}
+
+declare module '*.mp4' {
+  const src: string;
+  export default content;
+}
+declare module '*.webm' {
+  const src: string;
+  export default content;
+}
