41ff3575539204923d8217a5e706769a4226e564
rr <ringcrl@foxmail.com>
2025-10-13 16:54:45 +0800

    Update vite.config.ts

--- examples/vite-react/vite.config.ts
+++ examples/vite-react/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
   plugins: [react()],
 })
