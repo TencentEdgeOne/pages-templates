b99704217250907fdc101c91a4a5a563eedffb4b
allenxhu <allenxhu@tencent.com>
2025-10-23 19:56:26 +0800

    fix: deleted s3-batch-uploader

--- examples/s3-batch-uploader/app/[locale]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useEffect } from 'react'
-import { useRouter, redirect } from 'next/navigation'
-
-export default function HomePage({ params }: { params: { locale: string } }) {
-  const router = useRouter()
-  redirect('/en/upload')
-}
